f707bcd33af759d6c812e7bf9356f9ed197bb2f0
hangj97 <gksruwls@naver.com>
2023-03-03 03:08:41 +0900

    Part2 독립실행형 서블릿 애플리케이션

--- src/main/java/com/example/helloboot/HellobootApplication.java
+++ src/main/java/com/example/helloboot/HellobootApplication.java
@@ -1,13 +1,50 @@
 package com.example.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
-    }
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        // parameter -> 서블릿을 생성해보자.
+        // 익명 클래스를 통해 구현.
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
+                HelloController helloController = new HelloController();
 
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능 처리
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        String ret = helloController.hello(name);                                   // HelloController
+
+                        resp.setStatus(HttpStatus.OK.value());                                      // StatusCode 설정
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);       // 헤더 -> Content_Type 설정
+                        resp.getWriter().print(ret);                                                // Message Body 설정
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        // 404 처리
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+                }
+            }).addMapping("/*");                                                         // * -> 모든 것
+        });
+        webServer.start();
+    }
 }
